4bc0d8c9074bbf9003ed46847bc4d886b9c8a0a7
SonnyHardy <htambeck@gmail.com>
2024-07-19 20:36:18 +0200

    Projet initial

---
+++ src/main/java/com/sonny/ctcompte/CtCompteApplication.java
@@ -0,0 +1,13 @@
+package com.sonny.ctcompte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtCompteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtCompteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonny/ctcompte/CtCompteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonny.ctcompte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtCompteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
