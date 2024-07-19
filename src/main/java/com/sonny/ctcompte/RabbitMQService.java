02d22e6e27229d29d64de4384e7bbcc6735218de
SonnyHardy <htambeck@gmail.com>
2024-07-20 00:10:41 +0200

    Headers - envoie des messages

--- src/main/java/com/sonny/ctcompte/RabbitMQService.java
+++ src/main/java/com/sonny/ctcompte/RabbitMQService.java
@@ -5,20 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class RabbitMQService {
 
     @Autowired
     private RabbitTemplate template;
     private @Value("${application.exchanges.user.name}") String userExchangeName;
-    private @Value("${application.exchanges.user.routing-key}") String userExchangeRoutingKey;
-    private @Value("${application.exchanges.notification.name}") String notificationExchangeName;
-    private @Value("${application.exchanges.notification.routing-key}") String notificationExchangeRoutingKey;
 
 
     public void publier(Compte compte) {
-        this.template.convertAndSend(userExchangeName, userExchangeRoutingKey, compte);
-        this.template.convertAndSend(notificationExchangeName, notificationExchangeRoutingKey, compte);
+        this.template.convertAndSend(userExchangeName,
+                null,
+                compte,
+                messageProcessor -> {
+            final Map<String, String> headers = Map.of("type", "user", "action", "notification");
+            messageProcessor.getMessageProperties().getHeaders().putAll(headers);
+            return messageProcessor;
+                });
+
     }
 
 
