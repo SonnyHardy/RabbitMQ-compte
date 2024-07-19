497747d1889e892d4ede524ec148dfa6d49cd4c3
SonnyHardy <htambeck@gmail.com>
2024-07-19 23:20:29 +0200

    Direct - envoie des messages

--- src/main/java/com/sonny/ctcompte/RabbitMQService.java
+++ src/main/java/com/sonny/ctcompte/RabbitMQService.java
@@ -1,16 +1,25 @@
 package com.sonny.ctcompte;
 
-import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 public class RabbitMQService {
 
+    @Autowired
     private RabbitTemplate template;
+    private @Value("${application.exchanges.user.name}") String userExchangeName;
+    private @Value("${application.exchanges.user.routing-key}") String userExchangeRoutingKey;
+    private @Value("${application.exchanges.notification.name}") String notificationExchangeName;
+    private @Value("${application.exchanges.notification.routing-key}") String notificationExchangeRoutingKey;
+
 
     public void publier(Compte compte) {
-        this.template.convertAndSend(compte);
+        this.template.convertAndSend(userExchangeName, userExchangeRoutingKey, compte);
+        this.template.convertAndSend(notificationExchangeName, notificationExchangeRoutingKey, compte);
     }
+
+
 }
